8bf099bec44918377a7245935bfa3e7427739e26
cogmlek12-AFK <cogmlek12@gmail.com>
2026-09-08 17:46:47 +0900

    20260908 파이썬

--- 01_basic/04_operators.py
+++ 01_basic/04_operators.py
@@ -45,3 +45,51 @@ print(f"-5 <= a <= 5 결과: {-5 < a and a <= 5}")
 print(f"{-5 <= a <= 5}")
 
 # TODO : 멤버쉽 연산자 내일할고에용 
+
+print("=" * 60)
+print("멤버쉽 연산자(in), 식별 연산자(is)")
+print("=" * 60)
+
+members = ["김채희", "김동주", "박이안"]
+print(f"-> {members}")
+print(f" '김채희' 포함 여부 -> {'김채희' in members}")
+print(f" '임수진' 포함여부 -> {'임수진' in members}")
+
+print(f" '임수진' 포함하지않는지  -> {'임수진' not in members}")
+
+print(f"{'ll' in 'hello'}")
+print()
+
+x = [1,2,3]
+y = [1,2,3]
+z = x
+print(f"x : {x} / y : {y} / z : {z}")
+
+print(f"배열 값 비교 : {x == y}")
+print(f"객체 주소 비교 : {x is y}")
+print(f"x is z : {x is z}")
+
+ #None 비교 시 is 사용을 권장
+data = None
+print(f"data is none? {data is None}")
+print(f"data is not none? {data is None}")
+
+print("=" * 60)
+print("복합 대입 연산자")
+print("=" * 60)
+
+x = 10
+print(f"x : {x}")
+
+# x = x + 5
+x += 5 
+print(f"x += 5 : {x}")
+
+# x = x - 5
+x -= 5 
+print(f"x -= 5 : {x}")
+
+# 파이썬에서는 증감연산자 없음 대신에
+
+x += 1 # 1씩증가
+x -= 1 # 1씩감소
---
+++ 01_basic/05_condition.py
@@ -0,0 +1,87 @@
+
+print("=" * 60)
+print("if / elif / else")
+print("=" * 60)
+
+
+
+value = 10
+
+if value > 5:
+     print("조건문 내부입니다")
+     print("조건문 내에서 실행하고자 한다면 들여쓰기 필수")
+
+print("조건문 외부")
+print()
+
+score = int(input("점수입력 : "))
+
+if score >= 90:
+     grade = "A"
+elif score >= 80:
+     grade = "B"
+elif score >= 70:
+     grade = "C"
+else:
+     grade = "F"
+
+print(f"{score}점 => {grade}")
+# 블록은 들여쓰기로 구분, 조건식 옆에는 콜론(:) 지정
+# else if 대신 elif 사용
+
+print()
+
+print("=" * 60)
+print("삼항 연산")
+print("=" * 60)
+
+# 참일 때 결과값 if 조건식 else 거짓일 때 결과값 
+age = int (input("나이 입력 : "))
+"""
+if age >= 20:
+     result = "성인"
+else:
+     result = "미성년자"
+"""
+
+result = "성인" if age >= 20 else "미성년자"
+print(f"{age}세 -> {result}")
+
+print("=" * 60)
+print("match-case (java의 switch)")
+print("=" * 60)
+
+status = int(input("상태 코드 입력: "))
+
+match status:
+     case 200:
+          result = "정상"
+     case 404:
+          result = "페이지 찾을 수 없음"
+     case 500:
+          result = "서버 오류"
+     case _: 
+          result = "알 수 없음"
+print(f"{status} -> {result}")
+
+print("=" * 60)
+print(" pass ")
+print("=" * 60)
+
+# 빈 블록 작성할때사용 (오류방지)
+
+score = 90
+
+if score > 90:
+     pass
+else:
+     print("--- else영역 ---")
+
+n = 1
+
+while True:
+     if n > 3:
+          break # 반복문 종료
+     print(f"n : {n}")
+     n += 1
+
